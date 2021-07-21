b669a40f37b654d15628f6df37262dfdcff2e055
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2021-07-21 11:33:51 +0530

    (feature) Add Namespace (ns) Command

--- cmd/cli.go
+++ cmd/cli.go
@@ -1,14 +1,45 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"net"
 	"os"
 
 	"github.com/urfave/cli"
 )
 
 func main() {
-	err := cli.NewApp().Run(os.Args)
+	app := cli.NewApp()
+	app.Name = "Network Tools CLI"
+	app.Usage = "Let's you query IPs, CNAMEs, MX records and Name Servers!"
+
+	myFlags := []cli.Flag{
+		cli.StringFlag{
+			Name:  "host",
+			Value: "tutorialedge.net",
+		},
+	}
+
+	app.Commands = []cli.Command{
+		{
+			Name:  "ns",
+			Usage: "Looks for nameservers for a particular host",
+			Flags: myFlags,
+			Action: func(c *cli.Context) error {
+				ns, err := net.LookupNS(c.String("host"))
+				if err != nil {
+					return err
+				}
+				for i := 0; i < len(ns); i++ {
+					fmt.Println(ns[i].Host)
+				}
+				return nil
+			},
+		},
+	}
+
+	err := app.Run(os.Args)
 	if err != nil {
 		log.Fatal(err)
 	}
